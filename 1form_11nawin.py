81f4041aa83b09af99834f00493b6a23c00bada5
nawinRawmeat <n.hangsanawin@gmail.com>
2024-12-04 16:07:40 +0700

    Add files via upload

---
+++ 1form_11nawin.py
@@ -0,0 +1,9 @@
+print("หวัดดีค้าบผมนาวิน")
+id_acu = input("รหัสนักเรียน ")
+nickname = input('ชื่อเล่น')
+wish = input("อยากได้พรอะไร")
+
+print ("\n\n หวัดดีค้าบผม", nickname)
+print ("\n\n รหัสนักเรียนผมคิอ", id_acu)
+print ("\n\n อยากได้อะไร" , wish)
+print ("\n\n ขอให้สนุกกับพรแล้วไว้เจอกัน")
